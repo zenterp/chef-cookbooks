a62d3f0ef9185c403693fe054ade82b8eb483fd9
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-15 15:38:59 -0700

    [FEATURE] Add nginx cookbook, recipe and templates.

---
+++ nginx/recipes/default.rb
@@ -0,0 +1,26 @@
+package "nginx"
+
+service "nginx" do
+  action :enable
+end
+
+template "/etc/nginx/sites-available/default" do
+  source "sites-available.erb"
+end
+
+service "nginx" do
+  action :reload
+end
+
+directory "/var/www" do
+ owner "deploy"
+ action :create
+end
+
+template "/var/www/index.html" do
+  source "index.html.erb"
+end
+
+service "nginx" do
+  action :restart
+end
